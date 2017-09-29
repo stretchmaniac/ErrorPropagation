89300f0b17c985ff0d1490fcf00a3b2c114ac817
Alan Koval <stretchmaniac8@gmail.com>
2017-09-28 20:36:38 -0500

    Initial commit.

---
+++ Physics 1401V Lab 1.3.py
@@ -0,0 +1,112 @@
+from errorProp import Distribution
+import os.path
+import math
+
+# error for velocity, the average of the sum of the quotient of 6 different measured values
+
+vDist = None
+
+if os.path.exists('Physics 1401V Lab 1.3 data/v.txt'):
+	vDist = Distribution.fromFile('Physics 1401V Lab 1.3 data/v.txt')
+else:
+	d1 = Distribution.normalDistribution(.0245, .0012, 500)
+	d2 = d1.copy()
+	d3 = d1.copy()
+
+	t1 = Distribution.normalDistribution(.008518, .00001, 500)
+	t2 = Distribution.normalDistribution(.00829, .00001, 500)
+	t3 = Distribution.normalDistribution(.008256, .00001, 500)
+
+	quotients = Distribution.distZip([d1, d2, d3], [t1, t2, t3], lambda x,y:x/y)
+	sum = Distribution.foldIntoFirst(quotients, lambda x,y: x + y)
+
+	final = sum.singleArgCompute(lambda x: x/3)
+
+	final.draw()
+	bounds = final.getErrorBounds(1)
+	print(bounds)
+	print(bounds[1] - bounds[0], bounds[2] - bounds[1])
+			
+	final.show()
+	vDist = final
+	final.saveToFile('Physics 1401V Lab 1.3 data/v.txt')
+
+# now we get to data error measurements
+
+# copy-pasted from excel doc
+rawData = [
+	#	h_back	h_front	bar_len	h_lnch	h_cork	h1		h2		h3	
+	[	0,		0,		20.79,	1.029,	0.015,	1.711,	1.712,	1.711],
+	[	19.52,	22.8,	20.79,	1.031,	0.015,	1.827,	1.849,	1.872],
+	[	16.47,	23.3,	20.79,	1.033,	0.015,	1.902,	1.892,	1.918],
+	[	13.505,	23.8,	20.79,	1.039,	0.015,	1.963,	1.939,	1.958],
+	[	11,		24.24,	20.79,	1.048,	0.015,	1.858,	1.914,	1.836],
+	[	9.11,	24.8,	20.79,	1.048,	0.015,	1.752,	1.73,	1.746],
+	[	7.94,	25.3,	20.79,	1.036,	0.015,	1.624,	1.63,	1.59],
+	[	6.87,	25.9,	20.79,	1.036,	0.015,	1.396,	1.404,	1.378],
+	[	6.4,	26.6,	20.79,	1.036,	0.015,	1.114,	1.108,	1.114],
+	[	6.4,	27,		20.79,	1.04,	0.015,	1.036,	1.048,	1.04]
+]
+
+data = []
+for row in rawData:
+	data.append({
+		# name: [data, error]
+		'h_back':[row[0], .1],
+		'h_front':[row[1], .1],
+		'bar_len':[row[2], .1],
+		'h_launch':[row[3], .005],
+		'h_cork':[row[4], .005],
+		'hs':[[row[5], .005], [row[6], .005], [row[7], .005]],
+	})
+
+precision = 100
+errors = []
+for row in data:
+	errorRow = {}
+	# find error in calculated length from table, that is, L = sqrt(h_ave^2 - (h_launch - h_cork)^2)
+	hs = list(map(lambda x: Distribution.normalDistribution(x[0], x[1], precision), row['hs']))
+	# average of the hypotenuse
+	h_ave = Distribution.foldIntoFirst(hs, lambda x,y: x+y).singleArgCompute(lambda x: x / len(hs))
+	
+	height = Distribution.normalDistribution(*row['h_launch'], precision)
+	cork_height = Distribution.normalDistribution(*row['h_cork'], precision)
+	
+	intermediate = Distribution.computeDistribution(height, cork_height, lambda x,y: x - y)
+	lengthFromTable = Distribution.computeDistribution(h_ave, intermediate, lambda x,y: math.sqrt(x**2 - y**2))
+	
+	errorRow['L'] = lengthFromTable
+	
+	# theta error, where theta = asin((h_front - h_back) / bar_len))
+	h_front = Distribution.normalDistribution(*row['h_front'], precision)
+	h_back = Distribution.normalDistribution(*row['h_back'], precision)
+	bar_len = Distribution.normalDistribution(*row['bar_len'], precision)
+	
+	intermediate2 = Distribution.computeDistribution(h_front, h_back, lambda x,y: x - y)
+	theta = Distribution.computeDistribution(intermediate2, bar_len, lambda i, b: math.asin(i / b))
+	
+	errorRow['theta'] = theta
+	
+	# theoretical error, given by range = (v cos(theta) / g) (v sin(theta) + sqrt(v^2 sin(theta)^2 + 2 g h))
+	g = 9.8
+	theor1 = Distribution.computeDistribution(vDist, theta, lambda v,theta: v**2 * math.sin(theta)**2)
+	theor2 = Distribution.computeDistribution(theor1, h_ave, lambda th,h: math.sqrt(th + 2*g*h))
+	theor3 = Distribution.computeDistribution(vDist, theta, lambda v, theta: v*math.sin(theta))
+	theor4 = Distribution.computeDistribution(theor2, theor3, lambda x,y: x+y)
+	theor5 = Distribution.computeDistribution(vDist, theta, lambda v, t: v*math.cos(t) / g)
+	range = Distribution.computeDistribution(theor5, theor4, lambda x,y: x*y)
+	
+	errorRow['theoretical_range'] = range
+	
+	errors.append(errorRow)
+	
+for e in errors:
+	print(e['L'].getErrorBounds(1))
+	print(e['theoretical_range'].getErrorBounds(1))
+	
+	
+
+	
+
+
+
---
+++ errorProp.py
@@ -0,0 +1,407 @@
+import math
+from random import *
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+from matplotlib import collections  as mc
+
+class Distribution:
+	def __init__(self, intervals):
+		self.intervals = intervals
+		self.numPoints = len(intervals) + 1
+	
+	@staticmethod
+	def normalDistribution(center, standardDev, numPoints):
+		# there are alternatives to constant density in the x direction, but 
+		# 	for simplicity we'll stick to constant for now
+		varience = standardDev**2
+		minZ = -6
+		maxZ = 6
+		
+		intervals = []
+		
+		def zTrans(z):
+			return center + z*standardDev
+			
+		prevPoint = [zTrans(-6), 0]
+		
+		for i in range(numPoints):
+			x = zTrans(minZ) + (i+random()/10-.05) * (zTrans(maxZ) - zTrans(minZ)) / (numPoints - 1)
+			
+			# formula comes from wikipedia article on gaussian distribution
+			y = (1/math.sqrt(2*math.pi*varience))*math.e**(-(x-center)**2/(2*varience))
+			
+			intervals.append([prevPoint, [x,y]])
+			prevPoint = [x, y]
+			
+		return Distribution(intervals)
+		
+	@staticmethod
+	def vDistribution(leftRad, center, rightRad, numPoints):
+		xDelta = (rightRad - leftRad) / numPoints
+		
+		def plot(x):
+			if x <= center:
+				return 1-(center - x)/(center-leftRad)
+			return 1-(x - center)/(rightRad-center) 
+			
+		intervals = []
+		
+		# as in an inverted v, like a caret
+		for i in range(numPoints):
+			x = leftRad + (i+random()*.1-.05) * xDelta
+			nextX = leftRad + (i+random()*.1-.05+1) * xDelta
+			y = plot(x)
+			nextY = plot(nextX)
+			intervals.append([[x,y],[nextX, nextY]])
+		newDist = Distribution(intervals)
+		newDist.normalize()
+		return newDist
+		
+	@staticmethod
+	def computeDistribution(distA, distB, func):
+		globalIntervals = []
+		globalIntervalLengths = []
+		minX = None
+		maxX = None
+		for i1 in distA.intervals:
+			for i2 in distB.intervals:
+				# we can't assume that the first coordinate cooresponds to the first coordinate of the second point 7292752669
+				# we will find the widest interval (of all combos)
+				xs = None
+				try:
+					xs = (func(i1[0][0], i2[0][0]), func(i1[0][0], i2[1][0]), func(i1[1][0], i2[0][0]), func(i1[1][0], i2[1][0]))
+				except:
+					continue
+				
+				ys = (i1[0][1]*i2[0][1], i1[0][1]*i2[1][1], i1[1][1]*i2[0][1], i1[1][1]*i2[1][1])
+				
+				lowestIndex = None
+				lowest = None
+				highestIndex = None
+				highest = None
+				for i in range(len(xs)):
+					if lowestIndex == None or xs[i] < lowest:
+						lowestIndex = i
+						lowest = xs[i]
+					if highestIndex == None or xs[i] > highest:
+						highestIndex = i
+						highest = xs[i]
+				
+				interval = [[xs[lowestIndex], ys[lowestIndex]], [xs[highestIndex], ys[highestIndex]]]
+				
+				if interval[1][0] - interval[0][0] == 0:
+					print(i1[0][0], i1[1][0], i2[0][0], i2[1][0], interval[0][0], interval[1][0])
+				
+				for val in [interval[0][0], interval[1][0]]:
+					if minX == None or val < minX:
+						minX = val 
+					if maxX == None or val > maxX:
+						maxX = val
+				
+				globalIntervals.append(interval)
+				globalIntervalLengths.append((i1[1][0]-i1[0][0]) * (i2[1][0]-i2[0][0]))
+				
+		numPoints = max(distA.numPoints, distB.numPoints)
+		
+		condensed = Distribution.condenseIntervals(globalIntervals, globalIntervalLengths, minX, maxX, numPoints)
+		
+		intermediateDist = Distribution(condensed)
+		intermediateDist.normalize()
+		
+		newXLow = intermediateDist.integrateTo(.0001)
+		newXHigh = intermediateDist.integrateTo(.9999)
+		
+		condensed = Distribution.condenseIntervals(globalIntervals, globalIntervalLengths, newXLow, newXHigh, numPoints)
+		
+		newDist = Distribution(condensed)
+		newDist.normalize()
+		
+		newDist.smooth(3)
+		
+		return newDist
+	
+	# for functions like e^x
+	def singleArgCompute(self, func):
+		# map line segments, condense as before
+		newSegs = []
+		newSegLengths = []
+		xMin = None
+		xMax = None
+		for seg in self.intervals:
+			newSeg = [[func(seg[0][0]), seg[0][1]], [func(seg[1][0]), seg[1][1]]]
+			if newSeg[0][0] > newSeg[1][0]:
+				temp = newSeg[0]
+				newSeg[0] = newSeg[1]
+				newSeg[1] = temp
+				
+			for x in [newSeg[0][0], newSeg[1][0]]:
+				if xMin == None or x < xMin:
+					xMin = x
+				if xMax == None or x > xMax:
+					xMax = x
+			
+			newSegs.append(newSeg)
+			newSegLengths.append(seg[1][0] - seg[0][0])
+		
+		condensed = Distribution.condenseIntervals(newSegs, newSegLengths, xMin, xMax, self.numPoints)
+		
+		intermediateDist = Distribution(condensed)
+		intermediateDist.normalize()
+		
+		newXLow = intermediateDist.integrateTo(.0001)
+		newXHigh = intermediateDist.integrateTo(.9999)
+		
+		condensed = Distribution.condenseIntervals(newSegs, newSegLengths, newXLow, newXHigh, self.numPoints)
+		
+		newDist = Distribution(condensed)
+		newDist.normalize()
+		
+		newDist.smooth(3)
+		
+		return newDist
+		
+	@staticmethod
+	def condenseIntervals(globalIntervals, intervalLengths, minX, maxX, numPoints):
+		# condense into smaller set of intervals
+		finalIntervals = []
+		xDelta = (maxX - minX) / (numPoints)
+		
+		x = minX
+		while x < maxX-xDelta/100:
+			finalIntervals.append([[x,0],[x + xDelta, 0]])
+			x += xDelta
+			
+		def intervalIndex(xVal):
+			return math.floor((xVal - minX) / xDelta)
+			
+		def intervalOverlap(i1, i2):
+			left = None
+			right = None
+			if i1[0][0] < i2[0][0]:
+				left = i2[0][0]
+			else:
+				left = i1[0][0]
+			if i1[1][0] < i2[1][0]:
+				right = i1[1][0]
+			else:
+				right = i2[1][0]
+			return right - left if right > left else 0			
+		
+		# due to the fact that delta x is constant for distA and distB, the weight of each line segment is the same
+		# we will arbitrarily designate this weight 1
+		count = -1
+		for i in globalIntervals:
+			count += 1
+			# find range of intervals to which this interval extends
+			weightPerUnit = 1 / (i[1][0] - i[0][0])
+			intList = finalIntervals[intervalIndex(i[0][0]) : intervalIndex(i[1][0]) + 1]
+			
+			for iL in intList:
+				yDelta = intervalOverlap(iL, i) * .5 * (i[0][1] + i[1][1]) * weightPerUnit * intervalLengths[count]
+				iL[0][1] += yDelta 
+				iL[1][1] += yDelta
+		
+		# zip up the interval endpoints (e.g. make them the same as their neighbors)
+		i = 1
+		while i < len(finalIntervals):
+			aveY = .5*(finalIntervals[i-1][1][1] + finalIntervals[i][0][1])
+			finalIntervals[i-1][1][1] = aveY
+			finalIntervals[i][0][1] = aveY
+			i += 1
+			
+		return finalIntervals
+		
+	def normalize(self):
+		# makes the area under the curve 1 again
+		cumulativeArea = self.integrate(self.intervals[0][0][0], self.intervals[-1][1][0])
+		for i in self.intervals:
+			i[0][1] /= cumulativeArea 
+			i[1][1] /= cumulativeArea
+			
+	def draw(self):
+		lc = mc.LineCollection(self.intervals, linewidths=1)
+		fig, ax = plt.subplots()
+		ax.add_collection(lc)
+		ax.autoscale()
+		ax.margins(.1)
+		
+	def integrate(self, min, max):
+		return self.integrateLeft(max) - self.integrateLeft(min)
+		
+	def smooth(self, kernelRadius):
+		newIntervals = []
+		kernel = [1/(2*kernelRadius-1)] * (2*kernelRadius-1)
+		for i in range(len(self.intervals)):
+			count = 0
+			sum = 0
+			for j in range(-kernelRadius, kernelRadius + 1):
+				if 0 <= i + j < len(self.intervals):
+					sum += self.intervals[i + j][1][1]
+					count += 1
+			if count > 0:
+				newIntervals.append([[self.intervals[i][0][0], self.intervals[i][0][1]],[self.intervals[i][1][0], sum/count]])
+				
+		# reset first coordinate
+		for i in range(1, len(newIntervals)):
+			newIntervals[i][0][1] = newIntervals[i-1][1][1]
+			
+		self.intervals = newIntervals
+		
+	@staticmethod
+	def intervalArea(interval, prevI, nextI):
+		if prevI == None or nextI == None:
+			# default to a trapezoid approximation
+			return .5 * (interval[0][1] + interval[1][1]) * (interval[1][0] - interval[0][0])
+			
+		prevSlope = (prevI[1][1] - prevI[0][1]) / (prevI[1][0] - prevI[0][0])
+		nextSlope = (nextI[1][1] - nextI[0][1]) / (nextI[1][0] - nextI[0][0])
+			
+		prevX = (prevI[0][0] + prevI[1][0]) / 2
+		nextX = (nextI[0][0] + nextI[1][0]) / 2
+		
+		(px, py) = (prevX, (prevI[0][1] + prevI[1][1]) / 2)
+		a = (nextSlope - prevSlope) / (2*nextX - 2*prevX)
+		b = prevSlope - 2*a*prevX
+		c = py - a*px**2 - b*px
+		
+		# now integrate from interval[0][0] to interval[1][0]
+		# int(ax^2 + bx+c) = (1/3 ax^3 + 1/2 bx^2 + cx)
+		return (1/3) * a*interval[1][0]**3 + .5*b*interval[1][0]**2 + c*interval[1][0] - (1/3 * a*interval[0][0]**3+.5*b*interval[0][0]**2+c*interval[0][0])
+		
+	def integrateLeft(self, max):
+		area = 0
+		cInterval = None
+		prevInterval = None
+		c = 0
+		for interval in self.intervals:
+			currentInterval = interval
+			area += Distribution.intervalArea(currentInterval, prevInterval, self.intervals[c+1] if c + 1 < len(self.intervals) else None)
+			if currentInterval[1][0] > max:
+				break
+			prevInterval = interval
+			c += 1
+			
+		if currentInterval[1][0] > max:
+			area -= (currentInterval[1][0] - max) * (currentInterval[0][1] + currentInterval[1][1]) * .5
+			
+		return area
+		
+	def integrateTo(self, areaFromLeft):
+		# integrate from the side till we hit areaFromLeft
+		total = 0
+		currentInterval = None
+		c = 0
+		prevInterval = None
+		for interval in self.intervals:
+			currentInterval = interval
+			total += Distribution.intervalArea(interval, prevInterval, self.intervals[c+1] if c+1 < len(self.intervals) else None)
+			if total > areaFromLeft:
+				break
+			prevInterval = interval
+			c += 1
+		
+		if total > areaFromLeft:
+			return currentInterval[1][0] - (total - areaFromLeft) / (.5 * (currentInterval[0][1] + currentInterval[1][1]))
+		
+		return currentInterval[1][0]
+		
+	def getErrorBounds(self, standardDev):
+		area = norm.cdf(standardDev) - .5
+		
+		maxs = self.getLocalMaxima()
+		if len(maxs) > 0:
+			# get the highest maximum
+			maxs.sort(key = lambda x: x[1])
+			max = maxs[-1]
+			
+			middle = max[0]
+			middleArea = self.integrate(self.intervals[0][0][0], middle)
+			lower = self.integrateTo(middleArea - area)
+			upper = self.integrateTo(middleArea + area)
+			return (lower, middle, upper)
+		else:
+			raise RuntimeError("The resulting distribution appears to have no local maxima")
+		
+	def getLocalMaxima(self):
+		maxs = []
+		# use rolle's theorem to find intervals which contain a local maxima, then approximate the location
+		# using a quadratic regression of the slopes of the before and after intervals 
+		for i in range(1, len(self.intervals)-1):
+			prevI = self.intervals[i-1]
+			int = self.intervals[i]
+			nextI = self.intervals[i+1]
+			
+			prevSlope = (prevI[1][1] - prevI[0][1]) / (prevI[1][0] - prevI[0][0])
+			nextSlope = (nextI[1][1] - nextI[0][1]) / (nextI[1][0] - nextI[0][0])
+			
+			if prevSlope > 0 and nextSlope < 0:
+				# we have slope = d/dx ax^2 + bx + c = 2ax + b
+				prevX = (prevI[0][0] + prevI[1][0]) / 2
+				nextX = (nextI[0][0] + nextI[1][0]) / 2
+				
+				centerX = prevX + (nextX - prevX) * prevSlope / (prevSlope-nextSlope)
+				# we have s = 2ax + b, prevSlope = 2a(prevX) + b, nextSlope = 2a(nextX) + b => 
+				# prevSlope - 2a(prevX) = nextSlope - 2a(nextX), a = (nextSlope - prevSlope) / (2 nextX - 2 prevX)
+				# b = prevSlope - 2a(prevX)
+				# given a point (px, py)...
+				# a(px)^2+b(px)+c = py, c = py - a(px)^2 - b(px)
+				(px, py) = (prevX, (prevI[0][1] + prevI[1][1]) / 2)
+				a = (nextSlope - prevSlope) / (2*nextX - 2*prevX)
+				b = prevSlope - 2*a*prevX
+				c = py - a*px**2 - b*px
+				
+				centerY = a*centerX**2 + b*centerX + c
+				maxs.append([centerX, centerY])
+			
+		return maxs
+		
+	def copy(self):
+		intervalCopy = []
+		for i in self.intervals:
+			intervalCopy.append([[i[0][0], i[0][1]], [i[1][0], i[1][1]]])
+		newSelf = Distribution(intervalCopy)
+		newSelf.numPoints = self.numPoints
+		return newSelf
+	
+	# same as distZip(dists, copy(dists), func)
+	@staticmethod
+	def mapSelf(dists, func):
+		for i in range(len(dists)):
+			dists[i] = Distribution.computeDistribution(dists[i], dists[i].copy(), func)
+		
+	@staticmethod
+	def foldIntoFirst(dists, func):
+		result = dists[0]
+		i = 1
+		while i < len(dists):
+			result = Distribution.computeDistribution(result, dists[i], func)
+			i += 1
+		
+		return result
+		
+	def saveToFile(self, fileName):
+		file = open(fileName, 'w')
+		file.write(str(self.intervals))
+		file.close()
+	
+	@staticmethod 
+	def fromFile(fileName):
+		file = open(fileName, 'r')
+		return Distribution(eval(file.read()))
+	
+	# combines two lists of distributions elementwise according to func
+	@staticmethod
+	def distZip(dists1, dists2, func):
+		if len(dists1) != len(dists2):
+			raise Error('dist1 and dist2 must be the same length')
+		
+		res = []
+		for i in range(len(dists1)):
+			res.append(Distribution.computeDistribution(dists1[i], dists2[i], func))
+		
+		return res
+		
+	def show(self):
+		plt.show()
+
+		
